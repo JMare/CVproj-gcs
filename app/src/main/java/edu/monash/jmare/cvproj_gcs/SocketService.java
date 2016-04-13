498e69672f39b3d6c30b5ad655cd2277fe4b3b34
James <jamesmare12@gmail.com>
2016-04-13 21:42:39 +1000

    Major refactor: Change activity to a navigation bar slide out. Port functionality from old activity. Change entry point activity. Tested and still communicates.

--- app/src/main/java/edu/monash/jmare/cvproj_gcs/SocketService.java
+++ app/src/main/java/edu/monash/jmare/cvproj_gcs/SocketService.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
-import android.widget.Chronometer;
 import android.widget.Toast;
 
 import java.io.BufferedWriter;
